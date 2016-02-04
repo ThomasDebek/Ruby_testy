bcf76e7c225d154c3afd0fa4c443a26a515a39fa
ThomasDebek <tom.debek@gmail.com>
2016-02-04 22:33:56 +0100

    pliki

---
+++ czysty_ruby.rb
@@ -0,0 +1,46 @@
+puts "ala ma kota"
+
+
+x = "als ma kota"
+
+print x
+
+puts "Piko w oko"
+puts "Piko w oko"
+puts "Piko w oko"
+puts "Piko w oko"
+puts "Piko w oko"
+
+
+
+uczestnik = "Adam"
+zdnaie = "Klikaj"
+wyraz = "pan"
+
+puts wyraz +" "+ zdnaie +" "+ uczestnik
+
+
+polka = ["HObbit", "Potop", "kwiatek", 2, 5.5, "Rambo"]
+polka.class
+puts polka
+
+
+puts polka[3].class
+
+puts polka[2] = "Alchemik"
+puts polka
+
+polka.clear
+
+puts polka
+puts "a"
+
+koszyk = ["mleko", "cukier", ["bulka", "ciastko"] ]
+
+print koszyk[2][0]
+print koszyk.inspect
+
+print koszyk.join
+print koszyk.join(", ")
+
+
