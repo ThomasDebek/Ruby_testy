3ff55c86c901e9bbcb0d42137c9fa968066f470e
ThomasDebek <tom.debek@gmail.com>
2016-03-07 18:32:48 +0100

    Gra dodawanie

--- Gra_dodawanie.rb
+++ Gra_dodawanie.rb
@@ -1,12 +1,38 @@
 class GraDodawanie
   def initialize
     system "cls"   # czyli wyczyscmy ekran
+    @odpowiedz = ""
+    @punktacja = 0
     gra
   end
 
   def gra
     powitanie
-    koniec
+    until @odpowiedz == "koniec"
+      @a = rand(1..10)
+      @b = rand(11)
+      @wynik = @a + @b
+      @odpowiedz = gets.chomp
+
+      puts ""
+      print @imie + ", ile to jest #{@a} + #{@b} = ? "
+      puts ""
+      @odpowiedz = gets.chomp
+      puts "Twoja odpowiedz to: #{@odpowiedz}"
+
+      if @odpowiedz == @wynik.to_s
+        puts "dobra odpowiedz :)"
+        @punktacja +=1
+      elsif
+        @odpowiedz == "koniec"
+          koniec
+      else
+        puts "To nie jest dobra odpowiedz"
+        @punktacja -=1
+      end
+
+
+    end
   end
 
   def powitanie
@@ -16,10 +42,14 @@ class GraDodawanie
     puts "********************************"
     puts "Witaj #{@imie} w grze dodawanie".center(60)
     puts "********************************"
+    puts "Podpowiedz: jeśli chcesz zakączyć gre, wpisz słowo koniec"
   end
 
   def koniec
-
+    puts "Do zobaczenia " + @imie
+    puts "Twoj wynik to #{@punktacja} pkt"
+    puts "nacisnij [enter]"
+    gets
   end
 end
 
