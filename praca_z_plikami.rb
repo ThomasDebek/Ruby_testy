d85d198e915aca5b96c34963c07d4a2c079afad7
ThomasDebek <tom.debek@gmail.com>
2016-03-16 22:14:27 +0100

    Working with Files

---
+++ praca_z_plikami.rb
@@ -0,0 +1,12 @@
+# To co wyrużnia nas z pracą z plikami Windows and Unix jest separator
+
+# puts require '/Users/Tomasz/Applicationss_Rails/Strefakursow2015/Api/Ruby/Moduly.rb'  # Dla systemów UNIX
+# puts require '/Users/Tomasz/Applicationss_Rails/Strefakursow2015/Api/Ruby/Moduly.rb'  # Dla Windows
+
+
+puts require '/Users/Tomasz/Applicationss_Rails/Strefakursow2015/Api/Ruby/Moduly.rb'
+
+
+#puts File.join()
+
+p File.absolute_path(__FILE__)
